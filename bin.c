ca4b1c12929332ecb0d4fb864e7ad3699db58643
Travis Montoya <travistravgm.org>
2024-03-05 12:09:36 -0700

    binary search and cilk test

---
+++ bin.c
@@ -0,0 +1,54 @@
+/* bin.c
+ * Bentleys binary search adaption w/ Cilk and
+ * reading targets from a text file
+ * (C) Copyright 2024 Travis Montoya
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <cilk/cilk.h>
+#include "ctimer.h"
+
+#define bool_yn(x) (x > -1 ? 'y' : 'n')
+
+int
+bin_search(int *a, int k) {
+  int l = 0;
+  int u = 8;
+  int m;
+
+  while(l <= u) {
+    m = l + (u - l) / 2;
+    if (a[m] < k) {
+      l = m + 1;
+    } else if (a[m] == k) {
+      return m;
+    } else {
+      u = m - 1;
+    }
+  }
+  return -1;
+}
+
+int
+main(void) {
+  int i[9] = {1,2,2,3,6,7,8,9,20};
+  int n;
+
+  ctimer_t ct;
+  ctimer_start(&ct);
+
+  while(scanf("%d", &n) != EOF) {
+    char res;
+    cilk_scope {
+      res = cilk_spawn bool_yn(bin_search(i, n));
+    }
+    printf("%02d in array = %c\n", n, res);
+  }
+
+  ctimer_stop(&ct);
+  ctimer_measure(&ct);
+
+  ctimer_print(ct, "bin_search");
+
+  return 0;
+}
