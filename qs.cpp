136150680686a4fb1cd730d75430bec3a9ea9531
Travis Montoya <travistravgm.org>
2024-03-09 18:51:57 -0700

    updating qs

--- qs.cpp
+++ qs.cpp
@@ -3,22 +3,38 @@
 // A very practical in place sorting algorithm with worst case O(n^2) and an
 // expected running time of O(n log n)
 // Travis Montoya
-// Monday, May, 26, 2014
+// Monday, May, 26, 2014 - Initial implementation
+// Saturday, March, 09, 2024 - Fixed bugs, added large vector testing, timing
 ///////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
 
-using namespace std;
+#include "ctimer.h"
+
+/////////////////////////////////////////////////////////////////////////////////
+// Create a large unsorted vector
+std::vector<int> cvector(void) {
+    std::srand(std::time(nullptr));
+    std::vector<int> vec;
+
+    for (int i = 0; i < 5000; ++i) {
+        vec.push_back(std::rand() % 500 + 1);
+    }
+
+    return vec;
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 // Partition our vector for sorting
 template <typename T>
-unsigned int partition(vector<T>& a, unsigned int p, unsigned int r)
+unsigned int partition(std::vector<T>& a, unsigned int p, unsigned int r)
 {
-    auto x = a.at(r);          // Select a pivot element to sort vector
-    register auto i = p - 1;   // i must be p - 1 and j must equal p at
-    register decltype(i) j;    // start because no values are between i,p
+    auto x = a.at(r); // Select a pivot element to sort vector
+    auto i = p - 1;   // i must be p - 1 and j must equal p at
+    decltype(i) j;    // start because no values are between i,p
     
     // Reearrange a[p..r]
     for (j = p; j <= (r-1); j++)
@@ -26,10 +42,10 @@ unsigned int partition(vector<T>& a, unsigned int p, unsigned int r)
         if (a.at(j) <= x)
         {
             i++;
-            swap(a.at(i), a.at(j));
+            std::swap(a.at(i), a.at(j));
         }
     }
-    swap(a.at(i+1), a.at(r));
+    std::swap(a.at(i+1), a.at(r));
 
     // Return new index to quicksort
     return (i+1);
@@ -38,7 +54,7 @@ unsigned int partition(vector<T>& a, unsigned int p, unsigned int r)
 ///////////////////////////////////////////////////////////////////////////////
 // Sort our two subarrays
 template <typename T>
-vector<T> quicksort(vector<T>& a, unsigned int p, unsigned int r)
+std::vector<T> quicksort(std::vector<T>& a, unsigned int p, unsigned int r)
 {
     // A vector with 1 element is sorted already.
     if (a.size() <= 1) return (a);
@@ -54,14 +70,22 @@ vector<T> quicksort(vector<T>& a, unsigned int p, unsigned int r)
 
 int main()
 {
-    vector<int> x { 9,1,7,8,3,2,4,6,5,10,20,18,15,12,11,14,16,19,17,13,
-                    21,30,22,26,29,25,23,28,27,24,31,33,35,32,34 
-                  };
-
-    vector<int>::iterator xit = x.begin();
+    std::vector<int> x = cvector();
+    std::vector<int>::iterator xit = x.begin();
+    
+    ctimer_t ct;
+    ctimer_start(&ct);
 
     quicksort(x, 0, x.size()-1);
 
-    while(xit != x.end()) cout << *xit++ << " ";
-    cout << endl;
+    ctimer_stop(&ct);
+    ctimer_measure(&ct);
+
+    while(xit != x.end()) {
+      std::cout << *xit++ << " ";
+    }
+    std::cout << std::endl;
+
+    ctimer_print(ct, "qs");
+    return 0;
 }
